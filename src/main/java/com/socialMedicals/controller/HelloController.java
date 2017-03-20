986375e5e818915ebfa5f28938a2560a4a760168
LAPTOP-1206GM4S\yonay <yonaycl@gmail.com>
2017-03-20 12:16:43 +0000

    first commit

--- src/main/java/com/socialMedicals/controller/HelloController.java
+++ src/main/java/com/socialMedicals/controller/HelloController.java
@@ -2,16 +2,22 @@ package com.socialMedicals.controller;
 
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
-
-import static org.springframework.web.bind.annotation.RequestMethod.GET;
 
 @Controller
 public class HelloController {
 
     @RequestMapping("/index")
-    public String hello() {
+    public String index(Model model) {
+        model.addAttribute("message","esto es una prueba");
         return "index";
+    }
+
+    @RequestMapping("/hello")
+    public String hello() {
+        System.out.println("foo");
+        return "foo";
     }
 }
 
